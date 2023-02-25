8c68538472782bd6eaadb9fd905dba560a43b407
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-25 08:06:24 +0200

    another generic multi progress

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,61 @@
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
-use std::borrow::Cow;
 use std::thread;
 use std::time::Duration;
+
+enum TaskType {
+    Download,
+    Generic,
+}
 
 struct Task {
     progress_bar: ProgressBar,
     message: String,
-    task_fn: Box<dyn Fn(ProgressBar) + Send>,
+    task_type: TaskType,
 }
 
 impl Task {
-    fn new<F>(multi_progress: &MultiProgress, message: String, total: u64, task_fn: F) -> Self
-    where
-        F: 'static + Fn(ProgressBar) + Send,
-    {
+    fn new(
+        multi_progress: &MultiProgress,
+        message: String,
+        total: u64,
+        task_type: TaskType,
+    ) -> Self {
         let progress_bar = multi_progress.add(ProgressBar::new(total));
-        progress_bar.set_style(
-            ProgressStyle::default_bar()
+        let style = match task_type {
+            TaskType::Download => ProgressStyle::default_bar()
                 .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg}")
                 .unwrap()
                 .progress_chars("#>-"),
-        );
+            TaskType::Generic => ProgressStyle::default_spinner()
+                .template("{spinner:.green} {msg}")
+                .unwrap(),
+        };
+        progress_bar.set_style(style);
         progress_bar.set_message(message.clone());
 
         Self {
             progress_bar,
             message,
-            task_fn: Box::new(task_fn),
+            task_type,
         }
     }
 
     fn run(&self) {
-        (self.task_fn)(self.progress_bar.clone());
-        let message = format!("{} finished", self.message);
-        let message_cow: Cow<_> = message.into();
-        self.progress_bar.finish_with_message(message_cow);
+        match self.task_type {
+            TaskType::Download => {
+                for i in 0..self.progress_bar.length().unwrap() {
+                    self.progress_bar.set_position(i);
+                    thread::sleep(Duration::from_millis(50));
+                }
+            }
+            TaskType::Generic => {
+                self.progress_bar
+                    .enable_steady_tick(Duration::from_millis(100));
+                thread::sleep(Duration::from_secs(5));
+            }
+        }
+        self.progress_bar
+            .finish_with_message(format!("{} finished", self.message));
     }
 }
 
@@ -45,32 +66,17 @@ fn main() {
         &multi_progress,
         "Downloading file 1".to_string(),
         100,
-        |pb| {
-            for i in 0..100 {
-                pb.set_position(i);
-                thread::sleep(Duration::from_millis(50));
-            }
-        },
+        TaskType::Download,
     );
 
     let task2 = Task::new(
         &multi_progress,
         "Downloading file 2".to_string(),
         200,
-        |pb| {
-            for i in 0..200 {
-                pb.set_position(i);
-                thread::sleep(Duration::from_millis(25));
-            }
-        },
+        TaskType::Download,
     );
 
-    let task3 = Task::new(&multi_progress, "Task 3".to_string(), 50, |pb| {
-        for i in 0..50 {
-            pb.set_position(i);
-            thread::sleep(Duration::from_millis(100));
-        }
-    });
+    let task3 = Task::new(&multi_progress, "Task 3".to_string(), 0, TaskType::Generic);
 
     let tasks = vec![task1, task2, task3];
 
