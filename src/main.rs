ae15c8780c8bee1e16f2c57cebda4f994c1c1f4d
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-25 07:49:05 +0200

    beautiful example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use indicatif::{MultiProgress, ProgressBar};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use std::thread;
 use std::time::Duration;
 
@@ -6,14 +6,29 @@ fn main() {
     let multi_progress = MultiProgress::new();
 
     let pb1 = multi_progress.add(ProgressBar::new(100));
+    pb1.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg}")
+            .unwrap()
+            .progress_chars("#>-"),
+    );
+    pb1.set_message("Downloading file 1");
+
     let pb2 = multi_progress.add(ProgressBar::new(200));
+    pb2.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.green/black} {pos}/{len} {msg}")
+            .unwrap()
+            .progress_chars("#>-"),
+    );
+    pb2.set_message("Downloading file 2");
 
     let t1 = thread::spawn(move || {
         for i in 0..100 {
             pb1.set_position(i);
             thread::sleep(Duration::from_millis(50));
         }
-        pb1.finish();
+        pb1.finish_with_message("Downloaded file 1");
     });
 
     let t2 = thread::spawn(move || {
@@ -21,7 +36,7 @@ fn main() {
             pb2.set_position(i);
             thread::sleep(Duration::from_millis(25));
         }
-        pb2.finish();
+        pb2.finish_with_message("Downloaded file 2");
     });
 
     t1.join().unwrap();
